17c921d6c88caddc4748425169ce4df9072f3ca9
anthonyaltieri <Anthonyraltieri@gmail.com>
2016-11-06 01:04:29 -0700

    added combineGraves

--- src/Voodoo.js
+++ src/Voodoo.js
@@ -43,6 +43,29 @@ function perform(tag: Tag, payload: Payload) {
   helper(this.source, tag, payload);
 }
 
+function combineGraves(graves) {
+  if (typeof graves === 'undefined') {
+    throw new Error('The parameter must not be undefined');
+  }
+  if (typeof graves !== 'object') {
+    throw new Error('The parameter must be an object');
+  }
+  const keys = Object.keys(graves);
+  keys.forEach((k) => {
+    const value = graves[k];
+    if (typeof value === 'undefined') {
+      throw new Error('The values of the keys of the parameter must not be undefined');
+    }
+    if (typeof value !== 'function' && typeof value !== 'object') {
+      throw new Error('The value to the keys in the parameter object must be' +
+        'either a grave (Object) or a zombie (function)');
+    }
+  });
+  return {
+    ...graves
+  }
+};
+
 var Voodoo = {
   createSource,
   perform,
